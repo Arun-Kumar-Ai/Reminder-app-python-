a09a6285fed8aa6ca25281e288ff20e8a685df14
Arunsnlkumar <51483323+Arunsnlkumar@users.noreply.github.com>
2019-06-07 22:10:09 +0530

    Delete booty.py

--- App/booty.py
+++
@@ -1,9 +0,0 @@
-from app.event_controller import EventController
-
-class App:
-    def run(self):
-        self.run_controller()
-
-    def run_controller(self):
-        controller = EventController()
-        controller.list()
